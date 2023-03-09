90a6ee889c5d0accd7b7e605255d23ec5de20106
Ankit <ankitkansal2301@gmail.com>
2023-03-09 12:13:20 +0530

    Add all my files

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_ui/screens/weather_app.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+q
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WeatherApp(),
+    );
+  }
+}
+
+
+
